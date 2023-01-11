a8b8db1e8f27a4e71f18fe021edf3ecf863066df
Žydrūnas Puskunigis <puskunigiszydrunas@gmail.com>
2023-01-11 21:04:26 +0200

    Initializing spider, basic spider code added

---
+++ autoplius/autoplius/spiders/mercedes_benz_spider.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class MercedesBenzSpider(scrapy.Spider):
+    name = "mercedes_benz_c_class"
+    start_urls = ['https://autoplius.lt/skelbimai/naudoti-automobiliai?make_id=67&model_id=685']
+
+    def parse(self, response):
+        for car in response.css('div.announcement-body'):
+            yield {
+                'name': car.css('div.announcement-title').get(),
+                'price': car.css('div.announcement-pricing-info').get()
+            }
